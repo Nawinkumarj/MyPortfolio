2471cfe304f8279c2c73b7a5ba824935707d705d
Nawin Kumar <nawinkumar@trdfin.in>
2024-07-24 17:47:01 +0530

    Nawinkumar

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  mode: 'development',
   plugins: [react()],
   base: "/Myportfolio/", //Repository name
 })
